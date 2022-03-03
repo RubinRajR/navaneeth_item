a5ea9036831d5f331710705668eaca8be3df8de3
RubinRajR <rubinrao12005@gmail.com>
2022-03-03 15:38:32 +0530

    folder

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,30 @@
+package org.add;
+
+public class GreensTech {
+	private void greensOmr() {
+	System.out.println("teaching centre 000");
+}
+	private void greensAdayar() {
+System.out.println("teaching centre 001");
+	}
+	private void greensTambaram() {
+System.out.println("teaching centre 002");
+	}
+	private void greensVelacherry() {
+System.out.println("teaching centre 003");
+	}
+	private void greensAnnaNagar() {
+System.out.println("teaching centre 004");
+	}
+	
+	public static void main(String[] args) {
+		GreensTech c = new GreensTech();
+		c.greensOmr();
+		c.greensAdayar();
+		c.greensTambaram();
+		c.greensVelacherry();
+		c.greensAnnaNagar();
+		
+	}
+	
+}
